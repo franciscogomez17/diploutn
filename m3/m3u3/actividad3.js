298a2cd520c33f6b3f91e8cdbf255690724276d2
Francisco Adrian Gomez <franciscogomez.17@hotmail.com>
2025-11-20 15:59:56 -0300

    cambios

---
+++ m3/m3u3/actividad3.js
@@ -0,0 +1,11 @@
+let promedio;
+let sumanotas;
+let numero1 = 9;
+let numero2 = 7;
+
+sumanotas = numero1 + numero2;
+promedio = sumanotas/2;
+
+console.log(`El promedio de la materia historia es: ${promedio}`);
+alert(`El promedio de la materia historia es: ${promedio}`);
+
