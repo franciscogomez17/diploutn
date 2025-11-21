78d48efd18057f7a31d11908bf362d4b4b7cdb1a
Francisco Adrian Gomez <fran.gomez171106@gmail.com>
2025-11-20 21:41:56 -0300

    cambios

--- m3/m3u3/actividad3.js
+++ m3/m3u3/actividad3.js
@@ -1,6 +1,6 @@
 //Actividad 1
 function CalcularPromedio() {
-    sumanotas = nota1 + nota2;
+    let sumanotas = nota1 + nota2;
     let promedio = sumanotas / 2;
 
     return promedio;
@@ -15,9 +15,12 @@ let resultado = CalcularPromedio();
 console.log(`El promedio de la materia historia es: ${resultado}`);
 
 //Actividad 2
-const textarea = document.getElementById("mensaje");
-const contador = document.getElementById("contador");
+const texto = document.getElementById(`texto`);
 
-textarea.addEventListener("input", function () {
-    contador.textContent = textarea.value.length;
+const contar = document.getElementById(`contar`);
+
+//console.log(texto, contar);
+
+texto.addEventListener(`keyup`, function(){
+    contar.innerText = texto.value.length
 });
